8c38883fa04ee3505918b804835e51168fd548f1
Kevin Silha <ksilha@gmail.com>
2017-02-06 21:29:11 -0600

    Added user registration functionality

---
+++ app/src/main/java/com/se491/app/two2er/LoginActivity.java
@@ -0,0 +1,26 @@
+package com.se491.app.two2er;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class LoginActivity extends AppCompatActivity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        // setting default screen to login.xml
+        setContentView(R.layout.activity_login);
+
+        TextView registerScreen = (TextView) findViewById(R.id.link_to_register);
+
+        // Listening to register new account link
+        registerScreen.setOnClickListener(new View.OnClickListener() {
+
+            public void onClick(View v) {
+                startActivity(new Intent(LoginActivity.this, RegisterActivity.class));
+            }
+        });
+    }
+}
